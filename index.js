9e41a82d4948259195ce19247af9fb1cc199faea
pedro-krefta <pedro_hk_duarte@estudante.sesisenai.org.br>
2026-03-26 21:05:15 -0300

    v7

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 let des = document.getElementById('des').getContext('2d')
 
-let inimigo = new Inimigo(1900, 325, 150, 120, '../img/obstaculo01.png')
+let inimigo  = new Inimigo(1900, 325, 150, 120, '../img/obstaculo01.png')
 let inimigo2 = new Inimigo(1900, 125, 150, 120, '../img/obstaculo01.png')
 let inimigo3 = new Inimigo(1900, 400, 150, 120, '../img/obstaculo01.png')
 let inimigo4 = new Inimigo(1900, 200, 150, 120, '../img/obstaculo01.png')
-let player = new Player(100, 325, 150, 120, '../img/player01.png')
+let player  = new Player(100, 325, 150, 120, '../img/player01.png')
 let player2 = new Player(100, 325, 150, 120, '../img/player_02.png')
-
 let item = new Coletavel(1900, 100, 100, 80, '../img/coletavel.png')
 
+let bg1 = new Image()
+let bg2 = new Image()
+let bg3 = new Image()
+bg1.src = '../img/cenario01.png'
+bg2.src = '../img/cenario02.png'
+bg3.src = '../img/cenario03.jpg'
 
 let t1 = new Text()
 let t2 = new Text()
 let fase_txt = new Text()
+
+let barraVida1 = new BarraVida()
+let barraVida2 = new BarraVida()
 
 let boss = null
 let bossAtivo = false
@@ -20,294 +28,257 @@ let bossMorto = false
 let pontoBoss = 400
 let tirosBoss = []
 
-// let motor = new Audio('./img/motor.wav')
-// let batida = new Audio('./img/batida.mp3')
-// motor.volume = 0.5
-// motor.loop = true
-// batida.volume = 0.5
+let trilha      = new Audio('./img/trilha.mp3')
+let somTiro     = new Audio('./img/tiro.mp3')
+let somGameOver = new Audio('./img/gameOver.mp3')
+trilha.volume = 0.5
+trilha.loop = true
 
 let jogar = true
 let fase = 1
 let tiros = []
+let animFrameId = null
+let trilhaTocando = false  
 
-document.addEventListener('keydown', (e)=>{
-    if(e.key === 'w') player.dir = -10
-    if(e.key === 's') player.dir = 10
-    if(e.key === 'a') player.dir2 = -10
-    if(e.key === 'd') player.dir2 = 10
 
-    if(e.key === 'ArrowUp') player2.dir = -10
-    if(e.key === 'ArrowDown') player2.dir = 10
-    if(e.key === 'ArrowLeft') player2.dir2 = -10
-    if(e.key === 'ArrowRight') player2.dir2 = 10
+document.addEventListener('keydown', (e) => {
+  if (!trilhaTocando) {   
+        trilha.play()        
+        trilhaTocando = true 
+    }                        
+
+
+    if (e.key === 'w') player.dir = -10
+    if (e.key === 's') player.dir = 10
+    if (e.key === 'a') player.dir2 = -10
+    if (e.key === 'd') player.dir2 = 10
+
+    if (e.key === 'ArrowUp')    player2.dir  = -10
+    if (e.key === 'ArrowDown')  player2.dir  = 10
+    if (e.key === 'ArrowLeft')  player2.dir2 = -10
+    if (e.key === 'ArrowRight') player2.dir2 = 10
+
+    if (e.key === 'f' && player.vida > 0) {
+        tiros.push(new Tiro(player.x + player.w, player.y + player.h / 2, 20, 10, 'yellow'))
+        somTiro.currentTime = 0
+        somTiro.play()
+    }
+
+    if (e.key === 'Enter' && player2.vida > 0) {
+        tiros.push(new Tiro(player2.x + player2.w, player2.y + player2.h / 2, 20, 10, 'blue'))
+        somTiro.currentTime = 0
+        somTiro.play()
+    }
 })
 
-document.addEventListener('keyup', (e)=>{
-    if(e.key === 'w' || e.key === 's') player.dir = 0
-    if(e.key === 'a' || e.key === 'd') player.dir2 = 0
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'w' || e.key === 's') player.dir = 0
+    if (e.key === 'a' || e.key === 'd') player.dir2 = 0
 
-    
-    if(e.key === 'ArrowUp' || e.key === 'ArrowDown') player2.dir = 0
-    if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') player2.dir2 = 0
-
-    if(e.key === 'f') {
-        tiros.push(new Tiro(player.x + player.w, player.y + player.h/2, 10, 5, 'yellow'))
-    }
-
-    if(e.key === 'Enter') {
-        tiros.push(new Tiro(player2.x + player2.w, player2.y + player2.h/2, 10, 5, 'blue'))
-    }
+    if (e.key === 'ArrowUp'   || e.key === 'ArrowDown')  player2.dir  = 0
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') player2.dir2 = 0
 })
 
 function game_over() {
+    if (player.vida < 0)  player.vida  = 0
+    if (player2.vida < 0) player2.vida = 0
+
     if (player.vida <= 0 && player2.vida <= 0) {
         jogar = false
-        // motor.pause()
-        // música com o jogo parado
+        somGameOver.play()
+        desenha()
     }
 }
 
-function ver_fase() { 
-    if (player.pontos > 200 && fase === 1) {
+function ver_fase() {
+    let totalPontos = player.pontos + player2.pontos
+
+    if (totalPontos > 200 && fase === 1) {
         fase = 2
-        inimigo.vel = 3
+        inimigo.vel  = 3
         inimigo2.vel = 3
         inimigo3.vel = 3
-    } else if (player.pontos > 300 && fase === 2) {
+        inimigo4.vel = 3
+    } else if (totalPontos > 300 && fase === 2) {
         fase = 3
-    inimigo.vel = 5
-    inimigo2.vel = 5
-    inimigo3.vel = 5
-    inimigo4.vel = 5
-   
+        inimigo.vel  = 5
+        inimigo2.vel = 5
+        inimigo3.vel = 5
+        inimigo4.vel = 5
     }
-    
 }
 
 function colisao() {
-
     if (bossAtivo) return
 
+    const inimigos = [inimigo, inimigo2, inimigo3, inimigo4]
+    const players  = [player, player2]
 
-    if (player.colid(inimigo)) {
-        // batida.play()
-        inimigo.recomeca()
-        player.vida -= 1
+    players.forEach(p => {
+        if (p.vida <= 0) return
 
-    }
-    if (player.colid(inimigo2)) {
-        // batida.play()
-        inimigo2.recomeca()
-        player.vida -= 1
-    }
-    if (player.colid(inimigo3)) {
-        // batida.play()
-        inimigo3.recomeca()
-        player.vida -= 1
-    }
-    if (player.colid(inimigo4)) {
-        // batida.play()
-        inimigo4.recomeca()
-        player.vida -= 1
-    }
-    if (player2.colid(inimigo)) {
-        // batida.play()
-        inimigo.recomeca()
-        player2.vida -= 1
+        inimigos.forEach(ini => {
+            if (p.colid(ini)) {
+                ini.recomeca()
+                p.vida -= 1
+            }
+        })
 
-    }
-    if (player2.colid(inimigo2)) {
-        // batida.play()
-        inimigo2.recomeca()
-        player2.vida -= 1
-    }
-    if (player2.colid(inimigo3)) {
-        // batida.play()
-        inimigo3.recomeca()
-        player2.vida -= 1
-    }
-    if (player2.colid(inimigo4)) {
-        // batida.play()
-        inimigo4.recomeca()
-        player2.vida -= 1
-    }
-    if (player.colid(item)) {
-        // batida.play()
-        item.esconder()
-        player.vida += 1
-    }
-    if (player2.colid(item)) {
-        // batida.play()
-        item.esconder()
-        player2.vida += 1
-    }
-    // console.log('vida: ', player.vida)
+        if (p.colid(item)) {
+            item.esconder()
+            p.vida += 1
+        }
+    })
 }
 
 function pontuacao() {
-    if (player.point(inimigo) && !inimigo.pontuado) {
-        player.pontos -= 5
-        inimigo.pontuado = true
-        inimigo.recomeca()
-    }
-    if (player.point(inimigo2) && !inimigo2.pontuado) {
-        player.pontos -= 5
-        inimigo2.pontuado = true
-        inimigo2.recomeca()
-    }
-    if (player.point(inimigo3) && !inimigo3.pontuado) {
-        player.pontos -= 5
-        inimigo3.pontuado = true
-        inimigo3.recomeca()
-    }
-    if (player.point(inimigo4) && !inimigo4.pontuado) {
-        player.pontos -= 5
-        inimigo4.pontuado = true
-        inimigo4.recomeca()
-    }
+    const inimigos = [inimigo, inimigo2, inimigo3, inimigo4]
+
+    inimigos.forEach(ini => {
+        if (ini.x + ini.w < player.x && ini.x > -200 && !ini.pontuado) {
+            ini.pontuado = true
+        }
+    })
 }
 
 function desenharPlayers() {
-    if (player.vida > 0) player.des_carro()
+    if (player.vida > 0)  player.des_carro()
     if (player2.vida > 0) player2.des_carro()
 }
 
 function desenha() {
-    tirosBoss.forEach(t => t.desenhar())
+    des.clearRect(0, 0, 1920, 1080)
+
+    
+    if (fase === 1) des.drawImage(bg1, 0, 0, 1920, 1080)
+    else if (fase === 2) des.drawImage(bg2, 0, 0, 1920, 1080)
+    else if (fase === 3) des.drawImage(bg3, 0, 0, 1920, 1080)
+
     if (jogar) {
-        inimigo.des_carro()
-        inimigo2.des_carro()
-        inimigo3.des_carro()
-        inimigo4.des_carro()
+        if (!bossAtivo) {
+            inimigo.des_carro()
+            inimigo2.des_carro()
+            inimigo3.des_carro()
+            inimigo4.des_carro()
+        }
+
         item.des_carro()
         desenharPlayers()
+
         tiros.forEach(t => t.desenhar())
+        tirosBoss.forEach(t => t.desenhar())
+
         if (bossAtivo && boss) {
-        boss.des_carro()
+            boss.des_carro()
+           // fundo da barra
+des.fillStyle = '#333'
+des.fillRect(500, 20, 400, 20)
+
+// preenchimento
+des.fillStyle = 'purple'
+des.fillRect(500, 20, (boss.vida / 10) * 400, 20)
+
+// borda
+des.strokeStyle = 'white'
+des.lineWidth = 2
+des.strokeRect(500, 20, 400, 20)
+
+// texto
+t1.des_text('BOSS', 670, 55, 'white', 'bold 16px monospace')
         }
-        if (bossAtivo && boss) {
-        t1.des_text('BOSS VIDA: ' + boss.vida, 800, 80, 'red', '30px Arial')
+
+        if (bossMorto) {
+            t1.des_text('BOSS DERROTADO!', 450, 80, 'lime', '30px Arial')
         }
-        t1.des_text('Pontos: ' + player.pontos, 1000, 40, 'yellow', '26px Arial')
-        t2.des_text('Vidas: ' + player.vida, 40, 40, 'red', '26px Arial')
-        fase_txt.des_text('Fase: ' + fase, 550, 40, 'white', '26px Arial')
-        t2.des_text('Vidas: ' + player2.vida, 40, 80, 'red', '26px Arial')
-      
-    }else{
-        t1.des_text('GAME OVER', 450, 350, 'yellow', '60px Arial')
-        t2.des_text('Pontuação Final: ' + player.pontos, 480, 400, 'white', '25px Arial')
+
+        t1.des_text('P1 Pontos: ' + player.pontos,  1000, 40, 'yellow', '26px Arial')
+        barraVida1.desenhar(40, 50, player.vida,  5, '#ff4444', 'P1')
+        barraVida2.desenhar(40, 90, player2.vida, 5, '#44aaff', 'P2')
+        t2.des_text('P2 Pontos: ' + player2.pontos, 1000, 80, 'orange', '26px Arial')
+        fase_txt.des_text('Fase: ' + fase,           550,  40, 'white',  '26px Arial')
+
+    } else {
+        t1.des_text('GAME OVER', 450, 300, 'yellow', '60px Arial')
+        t2.des_text('P1 Pontuação Final: ' + player.pontos,  430, 380, 'white',  '25px Arial')
+        t2.des_text('P2 Pontuação Final: ' + player2.pontos, 430, 420, 'orange', '25px Arial')
     }
-
 }
 
-function colisao_tiro(){
-    tiros.forEach((t, i)=>{
+function colisao_tiro() {
+    const inimigos = [inimigo, inimigo2, inimigo3, inimigo4]
 
-        if(t.colide(inimigo)){
-            inimigo.recomeca()
-            tiros.splice(i,1)
-            player.pontos += 5
+    for (let i = tiros.length - 1; i >= 0; i--) {
+        let t = tiros[i]
+        let acertou = false
+
+        for (let ini of inimigos) {
+            if (t.colide(ini)) {
+                ini.recomeca()
+                if (t.a === 'yellow') player.pontos  += 5
+                else                  player2.pontos += 5
+                tiros.splice(i, 1)
+                acertou = true
+                break
+            }
         }
 
-        if(t.colide(inimigo2)){
-            inimigo2.recomeca()
-            tiros.splice(i,1)
-            player.pontos += 5
-        }
-
-        if(t.colide(inimigo3)){
-            inimigo3.recomeca()
-            tiros.splice(i,1)
-            player.pontos += 5
-        }
-
-        if(t.colide(inimigo4)){
-            inimigo4.recomeca()
-            tiros.splice(i,1)
-            player.pontos += 5
-        }
-
-        if (bossAtivo && boss && t.colide(boss)) {
+        if (!acertou && bossAtivo && boss && t.colide(boss)) {
             boss.levarDano()
-            tiros.splice(i,1)
+            if (t.a === 'yellow') player.pontos  += 10
+            else                  player2.pontos += 10
+            tiros.splice(i, 1)
         }
-        if(t.colide(inimigo)){
-            inimigo.recomeca()
-            tiros.splice(i,1)
-            player2.pontos += 5
-        }
-
-        if(t.colide(inimigo2)){
-            inimigo2.recomeca()
-            tiros.splice(i,1)
-            player2.pontos += 5
-        }
-
-        if(t.colide(inimigo3)){
-            inimigo3.recomeca()
-            tiros.splice(i,1)
-            player2.pontos += 5
-        }
-
-        if(t.colide(inimigo4)){
-            inimigo4.recomeca()
-            tiros.splice(i,1)
-            player2.pontos += 5
-        }
-
-        if (bossAtivo && boss && t.colide(boss)) {
-            boss.levarDano()
-            tiros.splice(i,1)
-        }
-
-
-    })
+    }
 }
 
-function colisao_tiroBoss(){
-    tirosBoss.forEach((t, i)=>{
-        if(t.colide(player)){
+function colisao_tiroBoss() {
+    for (let i = tirosBoss.length - 1; i >= 0; i--) {
+        let t = tirosBoss[i]
+
+        if (player.vida > 0 && t.colide(player)) {
             player.vida--
-            tirosBoss.splice(i,1)
+            tirosBoss.splice(i, 1)
+            continue
         }
-    })
+
+        if (player2.vida > 0 && t.colide(player2)) {
+            player2.vida--
+            tirosBoss.splice(i, 1)
+        }
+    }
 }
 
-
-
-function atualiza_tiros(){
-    tiros.forEach((t, i)=>{
-        t.mover()
-
-        if(t.x > 1920){
-            tiros.splice(i,1)
+function atualiza_tiros() {
+    for (let i = tiros.length - 1; i >= 0; i--) {
+        tiros[i].mover()
+        if (tiros[i].x > 1920) {
+            tiros.splice(i, 1)
         }
-    })
+    }
 }
 
-function atualiza_tirosBoss(){
-    tirosBoss.forEach((t, i)=>{
-        t.mover()
-
-        if(t.x < -50){
-            tirosBoss.splice(i,1)
+function atualiza_tirosBoss() {
+    for (let i = tirosBoss.length - 1; i >= 0; i--) {
+        tirosBoss[i].mover()
+        if (tirosBoss[i].x < -50) {
+            tirosBoss.splice(i, 1)
         }
-    })
+    }
 }
 
-function bossFight(){
-    if (player.pontos >= pontoBoss && !bossAtivo && !bossMorto) {
-        console.log("CRIANDO BOSS")
+function bossFight() {
+    let totalPontos = player.pontos + player2.pontos
+
+    if (totalPontos >= pontoBoss && !bossAtivo && !bossMorto) {
         boss = new Boss(1800, 200, 150, 150, '../img/boss.png', 10)
         bossAtivo = true
 
-        inimigo.x = -500
+        inimigo.x  = -500
         inimigo2.x = -500
         inimigo3.x = -500
         inimigo4.x = -500
     }
 }
-
 
 function atualizarBoss() {
     if (bossAtivo && boss) {
@@ -325,40 +296,33 @@ function atualizarInimigos() {
 }
 
 function atualizarPlayer1() {
-    if (player.vida > 0) {
-        player.mov_car()
-    }
+    if (player.vida > 0) player.mov_car()
 }
 
 function atualizarPlayer2() {
-    if (player2.vida > 0) {
-        player2.mov_car()
-    }
+    if (player2.vida > 0) player2.mov_car()
 }
 
-
-
 function atualiza() {
-    if (jogar) {
-        atualizarInimigos()
-        atualizarPlayer1()
-        atualizarPlayer2()
-        item.mov_car()
-        bossFight()
-        atualizarBoss()
-        atualiza_tirosBoss()
-        colisao_tiroBoss()
-        atualiza_tiros()
-        colisao_tiro()
-        colisao()
-        pontuacao()
-        ver_fase()
-        game_over()
-    }
+    if (!jogar) return
+
+    atualizarInimigos()
+    atualizarPlayer1()
+    atualizarPlayer2()
+    item.mov_car()
+    bossFight()
+    atualizarBoss()
+    atualiza_tirosBoss()
+    colisao_tiroBoss()
+    atualiza_tiros()
+    colisao_tiro()
+    colisao()
+    pontuacao()
+    ver_fase()
+    game_over()
 }
 
 function main() {
-    des.clearRect(0, 0, 1920, 1080)
     desenha()
     atualiza()
     requestAnimationFrame(main)
--- models/Carro.js
+++ models/Carro.js
@@ -1,5 +1,5 @@
-class Obj{
-    constructor(x,y,w,h,a){
+class Obj {
+    constructor(x, y, w, h, a) {
         this.x = x
         this.y = y
         this.w = w
@@ -10,48 +10,41 @@ class Obj{
         this.img.src = a
     }
 
-    des_carro(){
+    des_carro() {
         des.drawImage(this.img, this.x, this.y, this.w, this.h)
     }
 
-    
-
-    des_quad(){
+    des_quad() {
         des.fillStyle = this.a
-        des.fillRect(this.x, this.y, this.w, this.h,this.a)
+        des.fillRect(this.x, this.y, this.w, this.h)
     }
 
     des_carro_manual() {
-        // Roda dianteira esquerda (agora superior frente)
         des.beginPath()
         des.lineWidth = '5'
         des.strokeStyle = 'rgb(186, 186, 186)'
         des.fillStyle = 'darkorange'
-        des.rect(this.x + 60, this.y-50, 10, 10) 
+        des.rect(this.x + 60, this.y - 50, 10, 10)
         des.stroke()
         des.fill()
-    
-        // Roda dianteira direita (agora inferior frente)
+
         des.beginPath()
-        des.rect(this.x + 60, this.y-10, 10, 10)
+        des.rect(this.x + 60, this.y - 10, 10, 10)
         des.stroke()
         des.fill()
-    
-        // Roda traseira esquerda (agora superior trás)
+
         des.beginPath()
-        des.rect(this.x + 10, this.y-52, 10, 10)
+        des.rect(this.x + 10, this.y - 52, 10, 10)
         des.stroke()
         des.fill()
-    
-        // Roda traseira direita (agora inferior trás)
+
         des.beginPath()
-        des.rect(this.x + 10, this.y-8, 10, 10)
+        des.rect(this.x + 10, this.y - 8, 10, 10)
         des.stroke()
         des.fill()
-    
-        // Trapézio do corpo (Corpo principal rotacionado)
+
         des.beginPath()
-        des.moveTo(this.x, this.y - 50) 
+        des.moveTo(this.x, this.y - 50)
         des.lineTo(this.x, this.y)
         des.lineTo(this.x + 50, this.y - 10)
         des.lineTo(this.x + 50, this.y - 40)
@@ -61,22 +54,20 @@ class Obj{
         des.fillStyle = this.a
         des.stroke()
         des.fill()
-    
-        // Corpo frente (retângulo do "nariz")
+
         des.beginPath()
         des.rect(this.x + 50, this.y - 40, 20, 30)
         des.stroke()
         des.fill()
-    
-        // Asa frontal (vertical na ponta direita)
+
         des.beginPath()
         des.rect(this.x + 70, this.y - 50, 10, 50)
         des.stroke()
         des.fill()
-    }    
+    }
 }
 
-class Player extends Obj{
+class Player extends Obj {
 
     dir = 0
     dir2 = 0
@@ -85,128 +76,111 @@ class Player extends Obj{
     frame = 1
     tempo = 0
 
-    mov_car(){
+    mov_car() {
         this.y += this.dir
         this.x += this.dir2
-        // Limites da tela
-        if(this.y <= 0){
-            this.y = 0
-        }if(this.y >= 700){ // 1200 - 60 (largura do carro)
-            this.y = 700
-        }
-        
-        if(this.x <= 0){
-            this.x = 0
-        }if(this.x >= 1140){ // 1200 - 60 (largura do carro)
-            this.x = 1140
-        }
+
+        if (this.y <= 0) this.y = 0
+        if (this.y >= 700) this.y = 700
+        if (this.x <= 0) this.x = 0
+        if (this.x >= 1140) this.x = 1140
     }
 
-    colid(objeto){
-        if((this.x < objeto.x + objeto.w)&&
-          (this.x + this.w > objeto.x)&&
-          (this.y < objeto.y + objeto.h)&&
-          (this.y + this.h > objeto.y)){
+    colid(objeto) {
+        return (
+            this.x < objeto.x + objeto.w &&
+            this.x + this.w > objeto.x &&
+            this.y < objeto.y + objeto.h &&
+            this.y + this.h > objeto.y
+        )
+    }
+
+    point(objeto) {
+        if (objeto.x + objeto.w < this.x && objeto.x > -200) {
             return true
-        }else{
-            return false
         }
+        return false
     }
-
-    point(objeto){
-        if(objeto.x + objeto.w < this.x && objeto.x > -200){
-            return true
-        }else{
-            return false
-        }
-    }
-
-    
-
-    
 }
 
-class Inimigo extends Obj{
+class Inimigo extends Obj {
 
     vel = 4
 
-    recomeca(){
+    recomeca() {
         this.x = 1300
-        this.y =  Math.floor(Math.random() * (638 - 62) + 62)
+        this.y = Math.floor(Math.random() * (638 - 62) + 62)
         this.pontuado = false
     }
 
-    mov_car(){
+    mov_car() {
         this.x -= this.vel
-        if(this.x <= - 200){            
-            this.recomeca()         
+        if (this.x <= -200) {
+            this.recomeca()
         }
     }
 }
-class Coletavel extends Obj{
+
+class Coletavel extends Obj {
 
     vel = 10
     ativo = true
     tempoRespawn = 0
     tempoMax = 240
 
-    recomeca(){
+    recomeca() {
         this.x = 1300
         this.y = Math.floor(Math.random() * (650 - 62) + 62)
         this.pontuado = false
         this.ativo = true
     }
 
-    esconder(){
+    esconder() {
         this.x = -500
         this.ativo = false
         this.tempoRespawn = 0
     }
 
-    mov_car(){
-
-        if(this.ativo){
+    mov_car() {
+        if (this.ativo) {
             this.x -= this.vel
-
-            if(this.x <= -200){
+            if (this.x <= -200) {
                 this.esconder()
             }
-
         } else {
             this.tempoRespawn++
-
-            if(this.tempoRespawn >= this.tempoMax){
+            if (this.tempoRespawn >= this.tempoMax) {
                 this.recomeca()
             }
         }
     }
 }
 
-class Text{
-    des_text(text,x,y,cor,font){
+class Text {
+    des_text(text, x, y, cor, font) {
         des.fillStyle = cor
         des.lineWidth = '5'
         des.font = font
-        des.fillText(text,x,y)
+        des.fillText(text, x, y)
     }
 }
 
 class Tiro extends Obj {
 
-    vel = 10
-    w = 20
-    h = 10
-    a = 'red'
+    constructor(x, y, w, h, a) {
+        super(x, y, w || 20, h || 10, a || 'red')
+        this.vel = 10
+    }
 
-    mover(){
+    mover() {
         this.x += this.vel
     }
 
-    desenhar(){
+    desenhar() {
         this.des_quad()
     }
 
-    colide(obj){
+    colide(obj) {
         return (
             this.x < obj.x + obj.w &&
             this.x + this.w > obj.x &&
@@ -215,73 +189,103 @@ class Tiro extends Obj {
         )
     }
 }
+
 class Boss extends Obj {
 
-    vida = 10
-    tempoTiro = 0
-    dirY = 2 
+    constructor(x, y, w, h, a, vida) {
+        super(x, y, w, h, a)
+        this.vida = vida || 50
+        this.tempoTiro = 0
+        this.dirY = 2
+    }
 
-    atualizar(){
-
-        this.x = 1450
+    atualizar() {
+        if (this.x > 1100) {
+            this.x -= 2
+        }
 
         this.y += this.dirY
 
-        if(this.y <= 0){
+        if (this.y <= 0) {
             this.y = 0
             this.dirY *= -1
         }
-
-
-        if(this.y >= 700){
+        if (this.y >= 700) {
             this.y = 700
             this.dirY *= -1
         }
 
-
         this.tempoTiro++
-
-        if(this.tempoTiro > 120){
+        if (this.tempoTiro > 120) {
             this.tempoTiro = 0
-
-            let t = new TiroBoss()
-            t.x = this.x
-            t.y = this.y + this.h / 2
-
+            let t = new TiroBoss(this.x, this.y + this.h / 2, 20, 10, 'purple')
             tirosBoss.push(t)
         }
     }
 
-    levarDano(){
+    levarDano() {
         this.vida--
-
-        if(this.vida <= 0){
+        if (this.vida <= 0) {
             bossMorto = true
             bossAtivo = false
         }
     }
 }
+
 class TiroBoss extends Obj {
 
-    vel = -10
-    w = 20
-    h = 10
-    a = 'purple'
+    constructor(x, y, w, h, a) {
+        super(x, y, w || 20, h || 10, a || 'purple')
+        this.vel = -10
+    }
 
-    mover(){
+    mover() {
         this.x += this.vel
     }
 
-    desenhar(){
+    desenhar() {
         this.des_quad()
     }
 
-    colide(obj){
+    colide(obj) {
         return (
             this.x < obj.x + obj.w &&
             this.x + this.w > obj.x &&
             this.y < obj.y + obj.h &&
             this.y + this.h > obj.y
         )
+    }
+
+    
+}
+
+class BarraVida {
+    desenhar(x, y, vidaAtual, vidaMax, cor, label) {
+        const largura = 150
+        const altura = 14
+        const preenchimento = (vidaAtual / vidaMax) * largura
+
+        // sombra
+        des.shadowColor = cor
+        des.shadowBlur = 8
+
+        // fundo da barra
+        des.fillStyle = '#222'
+        des.fillRect(x, y, largura, altura)
+
+        // preenchimento
+        des.fillStyle = cor
+        des.fillRect(x, y, preenchimento, altura)
+
+        // borda
+        des.shadowBlur = 0
+        des.strokeStyle = 'white'
+        des.lineWidth = 1.5
+        des.strokeRect(x, y, largura, altura)
+
+        // label
+        des.fillStyle = 'white'
+        des.font = 'bold 13px monospace'
+        des.fillText(label + ' ' + vidaAtual + '/' + vidaMax, x, y - 5)
     }
 }
